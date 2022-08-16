9b54501eb5b6c509af2c6dab2765f1dc5e4d59a2
Rita Casiello <rituzka@gmail.com>
2022-08-16 21:20:38 +0200

    correccion

--- Ej_indiceMasaCorporal.py
+++ Ej_indiceMasaCorporal.py
@@ -1,10 +1,9 @@
 Python 3.10.6 (tags/v3.10.6:9c7b4bd, Aug  1 2022, 21:53:49) [MSC v.1932 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
-peso = 68.50
-estatura = 1.68
+peso = float(input('Cual es tu peso?'))
+Cual es tu peso? 68.50
+estatura = float(input('Cual es tu estatura?'))
+Cual es tu estatura? 1.68
 masaCorp = peso / (estatura * estatura)
-
-print("Tu indice de masa corporal es: "+ str(round(masaCorp,2)))
-Tu indice de masa corporal es: 24.27
-print("tu indice de masa corporal es: ", round(masaCorp,2))
-tu indice de masa corporal es:  24.27
+print('Tu indice de masa corporal es: ', round(masaCorp, 2))
+Tu indice de masa corporal es:  24.27
